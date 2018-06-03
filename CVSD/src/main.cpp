c4d08eb7e453538f17a93968816c2c105aefcfea
Florian <Florian@DESKTOP-7F6OTEM.Speedport_W_921V_1_44_000>
2018-06-03 13:16:44 +0200

    genClock() Funktion Timing korrigiert
    Ausgänge für XTAL und Encoder Data Clock in ISR implementiert;
    Taktfrequenzen verifiziert mit Waveforms/Analog Discovery

--- CVSD/src/main.cpp
+++ CVSD/src/main.cpp
@@ -9,9 +9,9 @@
 
 #define F_CPU 16000000UL
 #define TOGGLE_LED (PORTC ^= _BV(7)) //TOGGLE_MAKRO for Debugging PC7
-#define FX_XTAL 4					// FX609 XTALI (1), ATMEGA32u4 PB4 (IO8)
+#define FX_XTAL (PORTB ^= _BV(4))	// FX609 XTALI (1), ATMEGA32u4 PB4 (IO8)
 #define FX_ENC_OUT 7				// FX609 Encoder Ouptut (6), ATMEGA32u4 PB7 (IO11)
-#define FX_ENC_DCLK 6				// FX609 Encoder Data Clock (5), ATMEGA32u4 PD6 (IO12)
+#define FX_ENC_DCLK (PORTD ^= _BV(6))				// FX609 Encoder Data Clock (5), ATMEGA32u4 PD6 (IO12)
 
 
 #include "cvsd.h"
@@ -22,12 +22,14 @@ void SPI_MasterInit(void);
 
 int main(void){
 	DDRC = 0xff;		// Port C als Ausgang initialisieren
-
+	DDRB = 0x10;		// Pin PB4 als Ausgang initialisieren
+	DDRD = 0x40;		// Pin PD6 als Ausgang initialisieren
+	genClock(32000);
 	SPI_MasterInit();
+	sei();
 	while(1){
-//		TOGGLE_LED;  // Toggle PC7 z.B. angeschlossene LED
-//		_delay_ms(1000);       // Eine Sekunde warten...
-		genClock(16000);
+//		TOGGLE_LED;
+//		_delay_ms(1000);
 	}
 	return(0);
 }
@@ -36,7 +38,7 @@ int main(void){
 void SPI_MasterInit(void)
 {
 /* Set MOSI and SCK output, all others input */
-DDRB = (1<<PB2)|(1<<PB1);
+DDRB |= (1<<PB2)|(1<<PB1);
 /* Enable SPI, Master, set clock rate fck/16 */
 SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
 }
@@ -57,26 +59,50 @@ void Init_W5500(void)
 //while(!(SPSR & (1<<SPIF)));
 //}
 
+/* Die ISR erzeugt im Takt der genClock() Funktion an den Clockausgängen ein Rechtecksignal
+ * Das Makro FX_XTAL toggled den Ausgang PB4
+ * Das Makro FX_ENC_DCLK toggled den Ausgang PD6
+ *
+ */
+ISR(TIMER1_COMPA_vect){
+	FX_XTAL;
+	FX_ENC_DCLK;
+}
 
+/* void genClock(int _Rate) erzeugt mit TC1 ein Taktsignal in der Frequenz des Uebergabeparameters _Rate
+ * _Rate 16000 = 16kHz
+ * _Rate 32000 = 32kHz
+ * _Rate 64000 = 64kHz
+ * Auswertung der gewaehlten Frequenz über switch case Abfrage
+ * default: 16kHz
+ * Bei der Initialisierung des Timers wird der Compare Interrupt initialisiert und bei Erreichen die ISR
+ * ausgefuehrt
+ */
 void genClock(int _Rate){
 	switch (_Rate){
 	case 16000:
-		OCR1A = 0x3E8;				/**< Offsetwert laden: 16e6 / Prescaler / OCRA = 16e3 Hz */
+		OCR1A = 0x1F3;				/**< Offsetwert laden: 16e6 / Prescaler / OCRA = 16e3 Hz */
 		TCCR1B |= (1<<WGM12);		/**< Clear Timer on Compare */
 		TIMSK1 |= (1<<OCIE1A);		/**< enable interrupt */
 		TCCR1B |= (1<<CS10);
-		TOGGLE_LED;
-		_delay_ms(1000);       // Eine Sekunde warten...
 		break;
 	case 32000:
-		TOGGLE_LED;
-		_delay_ms(2000);       // Zwei Sekunden warten...
+		OCR1A = 0x0F9;				/**< Offsetwert laden: 16e6 / Prescaler / OCRA = 32e3 Hz */
+		TCCR1B |= (1<<WGM12);		/**< Clear Timer on Compare */
+		TIMSK1 |= (1<<OCIE1A);		/**< enable interrupt */
+		TCCR1B |= (1<<CS10);
 		break;
 	case 64000:
-		TOGGLE_LED;
-		_delay_ms(200);       // Eine halbe Sekunde warten...
+		OCR1A = 0x07C;				/**< Offsetwert laden: 16e6 / Prescaler / OCRA = 64e3 Hz */
+		TCCR1B |= (1<<WGM12);		/**< Clear Timer on Compare */
+		TIMSK1 |= (1<<OCIE1A);		/**< enable interrupt */
+		TCCR1B |= (1<<CS10);
 		break;
 	default:
+		OCR1A = 0x1F3;				/**< Offsetwert laden: 16e6 / Prescaler / OCRA = 16e3 Hz */
+		TCCR1B |= (1<<WGM12);		/**< Clear Timer on Compare */
+		TIMSK1 |= (1<<OCIE1A);		/**< enable interrupt */
+		TCCR1B |= (1<<CS10);
 		break;
 	}
 	return;
