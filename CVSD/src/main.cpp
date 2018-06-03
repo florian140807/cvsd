986972fe11c836cb4e6cccb89683a3d899f8dabc
Florian <Florian@DESKTOP-7F6OTEM.Speedport_W_921V_1_44_000>
2018-06-03 11:14:43 +0200

    Interface Pins ATMEGA32u4 to FX609 defined;
    genClock(): switch condition checked with TOGGLE_LED macro

--- CVSD/src/main.cpp
+++ CVSD/src/main.cpp
@@ -9,7 +9,9 @@
 
 #define F_CPU 16000000UL
 #define TOGGLE_LED (PORTC ^= _BV(7)) //TOGGLE_MAKRO for Debugging PC7
-
+#define FX_XTAL 4					// FX609 XTALI (1), ATMEGA32u4 PB4 (IO8)
+#define FX_ENC_OUT 7				// FX609 Encoder Ouptut (6), ATMEGA32u4 PB7 (IO11)
+#define FX_ENC_DCLK 6				// FX609 Encoder Data Clock (5), ATMEGA32u4 PD6 (IO12)
 
 
 #include "cvsd.h"
@@ -19,11 +21,13 @@ void SPI_MasterInit(void);
 //void Init_Socket(void);
 
 int main(void){
-	DDRC = 0xff;
+	DDRC = 0xff;		// Port C als Ausgang initialisieren
+
 	SPI_MasterInit();
 	while(1){
-		TOGGLE_LED;  // Toggle PC7 z.B. angeschlossene LED
-		_delay_ms(1000);       // Eine Sekunde warten...
+//		TOGGLE_LED;  // Toggle PC7 z.B. angeschlossene LED
+//		_delay_ms(1000);       // Eine Sekunde warten...
+		genClock(16000);
 	}
 	return(0);
 }
@@ -53,6 +57,7 @@ void Init_W5500(void)
 //while(!(SPSR & (1<<SPIF)));
 //}
 
+
 void genClock(int _Rate){
 	switch (_Rate){
 	case 16000:
@@ -60,10 +65,16 @@ void genClock(int _Rate){
 		TCCR1B |= (1<<WGM12);		/**< Clear Timer on Compare */
 		TIMSK1 |= (1<<OCIE1A);		/**< enable interrupt */
 		TCCR1B |= (1<<CS10);
+		TOGGLE_LED;
+		_delay_ms(1000);       // Eine Sekunde warten...
 		break;
 	case 32000:
+		TOGGLE_LED;
+		_delay_ms(2000);       // Zwei Sekunden warten...
 		break;
 	case 64000:
+		TOGGLE_LED;
+		_delay_ms(200);       // Eine halbe Sekunde warten...
 		break;
 	default:
 		break;
