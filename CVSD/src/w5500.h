5f75e625569f5d1451a69f3b435c773a9ff3d5f6
Florian <Florian@DESKTOP-7F6OTEM>
2018-06-07 06:08:49 +0200

    W5500.h defines
    Control Phase, BSB Mask für Sn0 hinzugefügt

--- CVSD/src/w5500.h
+++ CVSD/src/w5500.h
@@ -8,6 +8,12 @@
 #ifndef SRC_W5500_H_
 #define SRC_W5500_H_
 
+#define CP_BSB_CR 0x00000
+#define CP_BSP_S0 0x00001
+#define CP_BSP_S0_Tx 0x00010
+#define CP_BSP_S0_Rx 0x00011
+
+
 class w5500 {
 public:
 	w5500();
@@ -16,6 +22,7 @@ public:
 	void SetLayer3();
 	void SetLayer4();
 	void transmitChar(char _cData);
+	unsigned char receive()
 private:
 	unsigned char mac_addr[];
 	unsigned char ip_addr[];
