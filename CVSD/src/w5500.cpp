36f5249e572a81f4551290c3fa7735bc6c6d99e6
Florian <Florian@DESKTOP-7F6OTEM.Speedport_W_921V_1_44_000>
2018-06-17 16:50:13 +0200

    trouble shooting spi

--- CVSD/src/w5500.cpp
+++ CVSD/src/w5500.cpp
@@ -9,61 +9,89 @@
 
 w5500::w5500() {
 	/* Set MOSI and SCK output, all others input */
-	DDRB |= (1<<PB2)|(1<<PB1)|(1<<PB6);
+	DDRB |= (1<<PB2)|(1<<PB1);
 	/* Enable SPI, Master, set clock rate fck/16 */
 	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
 }
 // Set W5500 MAC Address
-void w5500::SetSHAR(unsigned char _a, unsigned char _b, unsigned char _c, unsigned char _d, unsigned char _e,
-		unsigned char _f){
-	mac_addr[5] = _a;
-	mac_addr[4] = _b;
-	mac_addr[3] = _c;
-	mac_addr[2] = _d;
-	mac_addr[1] = _e;
-	mac_addr[0] = _f;
+void w5500::SetSHAR(uint8_t _a, uint8_t _b, uint8_t _c, uint8_t _d, uint8_t _e, uint8_t _f){
+	SHAR[5] = _a;
+	SHAR[4] = _b;
+	SHAR[3] = _c;
+	SHAR[2] = _d;
+	SHAR[1] = _e;
+	SHAR[0] = _f;
 }
 //Set W5500 IP Address
-void w5500::SetSIPR(unsigned char _a, unsigned char _b, unsigned char _c, unsigned char _d){
-	ip_addr[3] = _a;
-	ip_addr[2] = _b;
-	ip_addr[1] = _c;
-	ip_addr[0] = _d;
+void w5500::SetSIPR(uint8_t _a, uint8_t _b, uint8_t _c, uint8_t _d){
+	SIPR[3] = _a;
+	SIPR[2] = _b;
+	SIPR[1] = _c;
+	SIPR[0] = _d;
 }
 //Set W5500 Subnetmask
-void w5500::SetSUBR(unsigned char _a, unsigned char _b, unsigned char _c, unsigned char _d){
-	sub_mask[3] = _a;
-	sub_mask[2] = _b;
-	sub_mask[1] = _c;
-	sub_mask[0] = _d;
+void w5500::SetSUBR(uint8_t _a, uint8_t _b, uint8_t _c, uint8_t _d){
+	SUBR[3] = _a;
+	SUBR[2] = _b;
+	SUBR[1] = _c;
+	SUBR[0] = _d;
 }
 //Set W5500 Gateway Address
-void w5500::SetGAR(unsigned char _a, unsigned char _b, unsigned char _c, unsigned char _d){
-	gtw_addr[3] = _a;
-	gtw_addr[2] = _b;
-	gtw_addr[1] = _c;
-	gtw_addr[0] = _d;
+void w5500::SetGAR(uint8_t _a, uint8_t _b, uint8_t _c, uint8_t _d){
+	GAR[3] = _a;
+	GAR[2] = _b;
+	GAR[1] = _c;
+	GAR[0] = _d;
 }
 //Set W5500 Source Port
-void w5500::SetPORT(unsigned char _a, unsigned char _b){
-	src_port[1] = _a;
-	src_port[0] = _b;
+void w5500::SetPORT(uint8_t _a, uint8_t _b){
+	PORT[1] = _a;
+	PORT[0] = _b;
 }
 //Set W5500 Destination Port
-void w5500::SetDstPORT(unsigned char _a, unsigned char _b){
-	src_port[1] = _a;
-	src_port[0] = _b;
+void w5500::SetDstPORT(uint8_t _a, uint8_t _b){
+	DstPORT[1] = _a;
+	DstPORT[0] = _b;
+}
+//Liefert Argumentenwert SHAR zurück
+uint8_t w5500::GetSHAR(uint8_t _a){
+	return SHAR[_a];
+}
+//Liefert Argumentenwert SIPR zurück
+uint8_t w5500::GetSIPR(uint8_t _a){
+	return SIPR[_a];
+}
+//Liefert Argumentenwert SUBR zurück
+uint8_t w5500::GetSUBR(uint8_t _a){
+	return SUBR[_a];
+}
+//Liefert Argumentenwert GAR zurück
+uint8_t w5500::GetGAR(uint8_t _a){
+	return GAR[_a];
+}
+//Liefert Argumentenwert PORT zurück
+uint8_t w5500::GetPORT(uint8_t _a){
+	return PORT[_a];
+}
+//Liefert Argumentenwert DstPORT zurück
+uint8_t w5500::GetDstPORT(uint8_t _a){
+	return DstPORT[_a];
 }
 
-unsigned char w5500::transmitChar(char _cData)
+
+void w5500::transmitChar(uint8_t _cData)
 {
 /* Start transmission */
 SPDR = _cData;
 /* Wait for transmission complete */
-//asm volatile("nop");
+asm volatile("nop");
 while(!(SPSR & (1<<SPIF)));
-return SPDR;
 }
+
+uint8_t w5500::receive(){
+	return SPDR;
+}
+
 w5500::~w5500() {
 	// TODO Auto-generated destructor stub
 }
