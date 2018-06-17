203459765b78245c34b31063ce991c4498642480
Florian <Florian@DESKTOP-7F6OTEM.Speedport_W_921V_1_44_000>
2018-06-17 09:46:23 +0200

    main.cpp
    W5500 Init Funktionen hinzugefügt

--- CVSD/src/main.cpp
+++ CVSD/src/main.cpp
@@ -16,12 +16,22 @@ int main(void){
 	clock myClock(64000);
 	w5500 myW5500;
 	serial myUART;
+	myW5500.SetSHAR(0x90, 0xA2, 0xDA, 0x11, 0x34, 0x30); // Init W5500 MAC
+	myW5500.SetSIPR(192, 168, 2, 144);
+	myW5500.SetGAR(192,168,2,1);
+	myW5500.SetSUBR(255,255,255,0);
+	myW5500.SetPORT(0xC3, 0x50);
+	myW5500.SetDstPORT(0xC3,0x51);
+	unsigned char readback;
 	sei();
 
 	while(1){
-
-		myUART.serial_send_int(myClock.getrate());
+		//readback = myW5500.transmitChar(myW5500.ip_addr[0]);
+		//myUART.serial_send_int(readback);
+		//readback = myW5500.transmitChar(myW5500.ip_addr[0]);
+		myUART.serial_send_byte(myClock.getrate());
 	}
 	return(0);
 }
+
 
