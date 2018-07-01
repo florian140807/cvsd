89dc5fd7d9b8d22927f5b397028fddcf05ed7520
Florian <Florian@DESKTOP-7F6OTEM.Speedport_W_921V_1_44_000>
2018-07-01 12:38:34 +0200

    cvsd.h
    delete FX_XTAL Macro, function is covered by FX_ENC_DCLK

--- CVSD/src/cvsd.h
+++ CVSD/src/cvsd.h
@@ -12,7 +12,6 @@
 #define BAUDRATE 9600
 #define BAUD_PRESCALER	(((F_CPU / (BAUDRATE * 16UL)))-1)
 #define TOGGLE_LED (PORTC ^= _BV(7)) //TOGGLE_MAKRO for Debugging PC7
-#define FX_XTAL (PORTB ^= _BV(4))	// FX609 XTALI (1), ATMEGA32u4 PB4 (IO8)
 #define FX_ENC_OUT 7				// FX609 Encoder Ouptut (6), ATMEGA32u4 PB7 (IO11)
 #define FX_ENC_DCLK (PORTD ^= _BV(6))				// FX609 Encoder Data Clock (5), ATMEGA32u4 PD6 (IO12)
 #define W5500_CS (PORTB &= ~(1<<PORTB6))		// W5500 Chip Select (Low active)
