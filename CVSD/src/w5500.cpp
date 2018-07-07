7d9f9b1be97ab8bc7d35285c24fcdcae7c8f66d3
Florian <Florian@DESKTOP-7F6OTEM.Speedport_W_921V_1_44_000>
2018-07-07 23:51:12 +0200

    w5500.cpp
    read and write functions added from manufacturer wiznet

--- CVSD/src/w5500.cpp
+++ CVSD/src/w5500.cpp
@@ -1,12 +1,61 @@
 #include "cvsd.h"
 
+//void W5500Class::init(void)
+//{
+//	_delay_ms(1000);
+//	initSS();
+//	resetSS();
+//	SPCR |= _BV(MSTR);
+//	SPCR |= _BV(SPE);
+//}
+
+
 void W5500Class::init(void)
 {
-	_delay_ms(1000);
-	initSS();
-	resetSS();
-	SPCR |= _BV(MSTR);
-	SPCR |= _BV(SPE);
+
+    initSS();
+    _delay_ms(300);
+    //delay(300);
+    //SPI.begin();
+    SPCR |= _BV(MSTR);
+    SPCR |= _BV(SPE);
+
+    for (int i=0; i<MAX_SOCK_NUM; i++) {
+        uint8_t cntl_byte = (0x0C + (i<<5));
+        write( 0x1E, cntl_byte, 2); //0x1E - Sn_RXBUF_SIZE
+        write( 0x1F, cntl_byte, 2); //0x1F - Sn_TXBUF_SIZE
+    }
+}
+
+uint16_t W5500Class::getTXFreeSize(SOCKET s)
+{
+    uint16_t val=0, val1=0;
+    do {
+        val1 = readSnTX_FSR(s);
+        if (val1 != 0)
+            val = readSnTX_FSR(s);
+    }
+    while (val != val1);
+    return val;
+}
+
+void W5500Class::send_data_processing(SOCKET s, const uint8_t *data, uint16_t len)
+{
+  // This is same as having no offset in a call to send_data_processing_offset
+  send_data_processing_offset(s, 0, data, len);
+
+}
+
+void W5500Class::send_data_processing_offset(SOCKET s, uint16_t data_offset, const uint8_t *data, uint16_t len)
+{
+
+    uint16_t ptr = readSnTX_WR(s);
+    uint8_t cntl_byte = (0x14+(s<<5));
+    ptr += data_offset;
+    write(ptr, cntl_byte, data, len);
+    ptr += len;
+    writeSnTX_WR(s, ptr);
+
 }
 
 uint8_t W5500Class::write(uint16_t _addr, uint8_t _cb, uint8_t _data)
@@ -56,5 +105,13 @@ uint16_t W5500Class::read(uint16_t _addr, uint8_t _cb, uint8_t *_buf, uint16_t _
     resetSS();
 
     return _len;
+}
+
+void W5500Class::execCmdSn(SOCKET s, SockCMD _cmd) {
+    // Send command to socket
+    writeSnCR(s, _cmd);
+    // Wait for command to complete
+    while (readSnCR(s))
+    ;
 }
 
