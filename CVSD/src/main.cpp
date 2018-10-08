ab65dd341820dd95fcfb25d083cfadecfd68bde0
Florian <Florian@DESKTOP-7F6OTEM.Speedport_W_921V_1_44_000>
2018-10-08 21:48:27 +0200

    main.cpp
    file cleaned up
    doxygen doc integrated
    comments reworked
    class name modified
    function InitIO() added
    function InitW5500LayerSettings() added
    function SetCurrentUtcTimeInUs() added

--- CVSD/src/main.cpp
+++ CVSD/src/main.cpp
@@ -1,78 +1,52 @@
-/*
- * main.cpp
+/**
+ * \file main.cpp
  *
- *  Created on: 30.03.2018
- *      Author: Florian
- *      20180409: move to git
- *      20180409: even more moved to online git
+ * \brief The main.cpp file
+ *
+ *  *
+ * \note KEEP IN MIND: W5500 uses Big Endian, where as Atmega32u4 uses
+ * Little Endian for word operations. For 16bit values, either define it already
+ * in Big Endian Order or doing a Byte Swapping
+ *
+ *
+ * \author  f. mertl
+ *
+ * \version
+ *
+ * \date 2018/10/08 14:16:20
+ *
+ * Contact: s140807@h-ab.de
+ *
+ * Created on: Wed Jun 03 18:39:37 2018
+ *
+ *
  */
-
 
 #include "cvsd.h"
 
 extern volatile bool bReady;
 extern volatile uint16_t writeIdx;
 extern volatile uint8_t buffer[];
+W5500Class W5500Chip;
+iena IenaPacket;
 
-uint16_t swapbytes(uint16_t wdat);
+
 
 int main(void){
-	PRR0 &= ~(1<<PRSPI);
-	DDRC = 0xC0;		// Pin PC7 und PC6 als Ausgang (LED) initialisieren
-	DDRB = 0x46;		// Pin PB6 als Ausgang initialisieren
-	DDRD = 0xC0;		// Pin PD7 und PD6 als Ausgang initialisieren
-	DDRE = 0x40;		// Pin PE6 als Ausgang initialisieren
-	uint16_t freesize = 0;
-	uint8_t mac_addr[] = {0x90,0xA2,0xDA,0x11,0x34,0x30};
-	//Network Setting for Home
-	uint8_t ip_addr[] = {192,168,2,144};
-	uint8_t remote_ip[] = {192,168,2,114};
-	uint8_t sub_mask[] = {255,255,255,0};
-	uint8_t gtw_addr[] = {192,168,2,1};
-	//Network Settings for Office
-//	uint8_t ip_addr[] = {10,253,1,144};
-//	uint8_t remote_ip[] = {10,253,255,255};
-//	uint8_t sub_mask[] = {255,255,0,0};
-//	uint8_t gtw_addr[] = {10,253,1,253};
-	uint16_t sPort = 50000;
-	uint16_t dstPort = 50001;
-	serial myUART;
-	iena myIENA;
-	enc_clock myClock(FS);
-	W5500Class myW5500;
-	myW5500.init();
-	myW5500.writeMR(MR::RST);
-	myW5500.writeGAR(gtw_addr);
-	myW5500.writeSHAR(mac_addr);
-	myW5500.writeSIPR(ip_addr);
-	myW5500.writeSUBR(sub_mask);
-	while(myW5500.readSnSR(0)!=SnSR::UDP){
-		myW5500.writeSnMR(0,SnMR::UDP);
-		myW5500.writeSnPORT(0,sPort);
-		myW5500.writeSnCR(0,Sock_OPEN);
-	}
-	freesize = myW5500.getTXFreeSize(0);
-	while(freesize < sizeof(myIENA)) freesize = myW5500.readSnTX_FSR(0);
-	myW5500.writeSnDIPR(0,remote_ip);
-	myW5500.writeSnDPORT(0,dstPort);
-	sei();
 	uint16_t readIdx = 0;
-	uint16_t rDay = 269;
-	uint8_t rhour = 12;
-	uint8_t rmin = 34;
-	uint8_t rsec = 30;
-	uint64_t ll_hdr_time = (rDay*24)+rhour;
-	ll_hdr_time = (ll_hdr_time * 60)+rmin;
-	ll_hdr_time = (ll_hdr_time * 60)+rsec;
-	ll_hdr_time = ll_hdr_time * 1e6;
 	bool match = 0;
+	uint64_t ll_hdr_time = SetCurrentUtcTimeInUs(281,15,33,45);
+	PRR0 &= ~(1<<PRSPI);
+	InitIO();
+	InitW5500LayerSettings();
+	enc_clock EncoderSamplingClock(FS);
+	sei();
 	while(1){
 		if(readIdx == writeIdx){
 			match=1;
 		}
 			else{
 				match=0;
-
 		}
 		switch (bReady){
 		case 0: break;
@@ -80,17 +54,16 @@ int main(void){
 			switch(match){
 			case (1):break;
 			case (0):
-					myIENA.SetPayload(readIdx,buffer[readIdx]);
+					IenaPacket.SetPayload(readIdx,buffer[readIdx]);
 					readIdx++;
 					readIdx &= BUFMSK;
-					TOGGLE3;
 					switch(readIdx){
 					case (BYTESPERPACKET):
-							myW5500.send_data_processing(0,(uint8_t *) &myIENA,sizeof(myIENA));
-							myW5500.writeSnCR(0,Sock_SEND);
-							myIENA.IncSequence();
+							W5500Chip.send_data_processing(0,(uint8_t *) &IenaPacket,sizeof(IenaPacket));
+							W5500Chip.writeSnCR(0,Sock_SEND_MAC);
+							IenaPacket.IncSequence();
 							ll_hdr_time = ll_hdr_time + TIMECOUNTINC;
-							myIENA.SetIENATime(ll_hdr_time);
+							IenaPacket.SetIENATimeInUs(ll_hdr_time);
 							readIdx=0;
 							writeIdx=0;
 							break;
@@ -106,9 +79,46 @@ int main(void){
 	return(0);
 	}
 
-uint16_t swapbytes(uint16_t wdat){
-	wdat= (wdat>>8)|((wdat & 0xff)<<8);
-	return wdat;
+void InitIO(void){
+	DDRB = 0x46;
+	DDRD = 0x40;
+}
+
+void InitW5500LayerSettings(void){
+	uint16_t freesize = 0;
+	uint8_t mac_addr[] = {0x90,0xA2,0xDA,0x11,0x34,0x30};
+	uint8_t dmac_addr[] = {0x01,0x00,0x5E,0x76,0xFD,0x11};
+	uint8_t ip_addr[] = {10,253,28,1};
+	uint8_t dip_addr[] = {236,253,0,1};
+	uint8_t sub_mask[] = {255,255,0,0};
+	uint8_t gtw_addr[] = {192,168,2,1};
+	uint16_t port = SOURCEPORT;
+	uint16_t dport = DESTINATIONPORT;
+	W5500Chip.init();
+	W5500Chip.writeMR(MR::RST);
+	W5500Chip.writeSnDHAR(0,dmac_addr);
+	W5500Chip.writeGAR(gtw_addr);
+	W5500Chip.writeSHAR(mac_addr);
+	W5500Chip.writeSIPR(ip_addr);
+	W5500Chip.writeSUBR(sub_mask);
+	W5500Chip.writeSnMR(0,SnMR::MULTI);
+	W5500Chip.writeSnDPORT(0,dport);
+	W5500Chip.writeSnDIPR(0,dip_addr);
+	while(W5500Chip.readSnSR(0)!=SnSR::UDP){
+		W5500Chip.writeSnMR(0,SnMR::UDP);
+		W5500Chip.writeSnPORT(0,port);
+		W5500Chip.writeSnCR(0,Sock_OPEN);
+	}
+	freesize = W5500Chip.getTXFreeSize(0);
+	while(freesize < sizeof(IenaPacket)) freesize = W5500Chip.readSnTX_FSR(0);
+}
+
+uint64_t SetCurrentUtcTimeInUs(uint16_t _doy, uint8_t _hour, uint8_t _min, uint8_t _sec){
+	uint64_t UtcHdrTime = (_doy*24)+_hour;
+	UtcHdrTime = (UtcHdrTime * 60)+_min;
+	UtcHdrTime = (UtcHdrTime * 60)+_sec;
+	UtcHdrTime = UtcHdrTime * 1e6;
+	return UtcHdrTime;
 }
 
 
